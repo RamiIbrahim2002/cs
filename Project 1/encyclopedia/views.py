f4cfcc766cc6ffebfd773f3ca1fd8178f4310b8b
unknown <ramybrahim77@gmail.com>
2023-12-07 20:46:01 +0100

    done with the search section & started with the create a new page

--- Project 1/encyclopedia/views.py
+++ Project 1/encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from . import util
 
@@ -9,14 +9,34 @@ def index(request):
     })
 
 def title(request, title):
-    entries = util.list_entries()
-    if title in entries : 
-        return render(request, "encyclopedia/article.html", {
-                "name": title,
-                "markdown": util.convert_to_html(util.get_entry(title))
-            })
-    else :
-        return render(request, "encyclopedia/error.html", {
-            "error": 'no such article',
-        })
+    #entries = util.list_entries()
+    for entry in util.list_entries():
+        print(title.upper())
+        print(entry.upper())
+        if title.upper() == entry.upper() : 
+            return render(request, "encyclopedia/article.html", {
+                    "name": title,
+                    "markdown": util.convert_to_html(util.get_entry(title))
+                })
+    return render(request, "encyclopedia/error.html", {
+        "error": 'no such article',
+    })
 
+def search(request):
+    searched = request.GET['q']
+    for entry in util.list_entries():
+        #print(entry.upper())
+        if entry.upper() == searched.upper() :
+            return redirect("entry" , title = searched)
+    else:
+        results = []
+        for entry in util.list_entries():
+            #print(searched in entry)
+            if searched.upper() in entry.upper():
+                results.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "results": results
+        })
+    
+def create(request):
+    return render(request,"encyclopedia/create.html")
